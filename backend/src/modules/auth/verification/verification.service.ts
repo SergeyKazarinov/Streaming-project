f1a8198bdd0922665c17304db3d10f4652b61cdf
Sergey Kazarinov <kazarinov092@gmail.com>
2026-01-20 21:24:39 +0500

    refactor: create check-token util

--- backend/src/modules/auth/verification/verification.service.ts
+++ backend/src/modules/auth/verification/verification.service.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Request } from 'express';
 import { User } from 'prisma/generated/prisma/client';
 import { TokenType } from 'prisma/generated/prisma/enums';
 
 import { MailService } from '@/modules/mail/mail.service';
 
+import { checkToken } from '@/shared/lib/check-token.util';
 import { generateToken } from '@/shared/lib/generate-token.util';
 import { prisma } from '@/shared/lib/prisma';
 import { saveSession } from '@/shared/lib/session.util';
@@ -19,22 +20,7 @@ export class VerificationService {
   async verify(req: Request, input: VerificationInput, userAgent: string) {
     const { token } = input;
 
-    const existingToken = await prisma.token.findUnique({
-      where: {
-        token,
-        type: TokenType.EMAIL_VERIFY,
-      },
-    });
-
-    if (!existingToken) {
-      throw new NotFoundException('Токен не найден');
-    }
-
-    const hasExpired = new Date(existingToken.expiresIn) < new Date();
-
-    if (hasExpired) {
-      throw new BadRequestException('Токен истек');
-    }
+    const existingToken = await checkToken(token, TokenType.EMAIL_VERIFY);
 
     const user = await prisma.user.update({
       where: { id: existingToken.userId },
