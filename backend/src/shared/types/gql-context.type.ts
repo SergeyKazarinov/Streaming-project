0bac3640a50f4e786483e71c0754673d2fa3601f
Sergey Kazarinov <kazarinov092@gmail.com>
2026-01-25 19:34:02 +0500

    types: add global.d: Nullable type

--- backend/src/shared/types/gql-context.type.ts
+++ backend/src/shared/types/gql-context.type.ts
@@ -2,6 +2,6 @@ import type { Request, Response } from 'express';
 import type { User } from 'prisma/generated/prisma/client';
 
 export interface GqlContext {
-  req: Request & { user: User | null };
+  req: Request & { user: Nullable<User> };
   res: Response;
 }
