37299c943861db9f0c3fa77dc9637809782d7d5a
Sergey Kazarinov <kazarinov092@gmail.com>
2026-07-18 13:15:53 +0500

    feat(db): add seed for the categories, users, streams

---
+++ backend/src/core/prisma/category.entries.ts
@@ -0,0 +1,87 @@
+export const CATEGORIES = [
+  {
+    title: 'Шутеры',
+    slug: 'shooters',
+    description:
+      'Категория для поклонников динамичных шутеров от первого и третьего лица. Здесь стримят такие игры, как Counter-Strike, Call of Duty, Apex Legends, Valorant и другие.',
+    thumbnailUrl: '/categories/shooters.webp',
+  },
+  {
+    title: 'MOBA',
+    slug: 'moba',
+    description:
+      'Массовые многопользовательские онлайн битвы на аренах: Dota 2, League of Legends, Smite и тому подобное. Объединяйтесь в команды и боритесь за победу!',
+    thumbnailUrl: '/categories/moba.webp',
+  },
+  {
+    title: 'RPG',
+    slug: 'rpg',
+    description:
+      'Ролевые игры в одиночку и с друзьями: The Witcher, Skyrim, Final Fantasy, Baldur’s Gate. Погружение в увлекательные миры и развитие персонажей.',
+    thumbnailUrl: '/categories/rpg.webp',
+  },
+  {
+    title: 'Симуляторы и стратегии',
+    slug: 'strategy-simulation',
+    description:
+      'Лучшие стратегии и симуляторы: от строительства городов до управления армиями и экономикой. Civilization, The Sims, Cities: Skylines, StarCraft II и многие другие.',
+    thumbnailUrl: '/categories/strategy-simulation.webp',
+  },
+  {
+    title: 'Приключения и платформеры',
+    slug: 'adventure-platformer',
+    description:
+      'Захватывающие приключения, головоломки и расслабляющие платформеры: Ori and the Blind Forest, Hollow Knight, Limbo, Little Nightmares.',
+    thumbnailUrl: '/categories/adventure-platformer.webp',
+  },
+  {
+    title: 'Киберспорт',
+    slug: 'esports',
+    description:
+      'Стримы профессиональных турниров и матчей по киберспортивным дисциплинам: CS:GO, Dota 2, League of Legends, Valorant, Overwatch и других.',
+    thumbnailUrl: '/categories/esports.webp',
+  },
+  {
+    title: 'IRL (В реальной жизни)',
+    slug: 'irl',
+    description:
+      'Общение, путешествия, фриланс, кулинария и всё, что происходит вне виртуальных миров. Стримеры показывают свою повседневную жизнь, делятся эмоциями и опытом.',
+    thumbnailUrl: '/categories/irl.webp',
+  },
+  {
+    title: 'Музыка и выступления',
+    slug: 'music-performing',
+    description: 'Творческие стримы: живые выступления, диджеинг, уроки музыки, пение, каверы и многое другое.',
+    thumbnailUrl: '/categories/music-performing.webp',
+  },
+  {
+    title: 'Креатив',
+    slug: 'creative',
+    description: 'Рисование, моделирование, рукоделие, 3D-графика, ручная работа и всё, что связано с креативом.',
+    thumbnailUrl: '/categories/creative.webp',
+  },
+  {
+    title: 'Обучение и наука',
+    slug: 'education-science',
+    description: 'Стримы о науке, обучении, популяризации знаний, программировании, математике и многом другом.',
+    thumbnailUrl: '/categories/education-science.webp',
+  },
+  {
+    title: 'Авто и техника',
+    slug: 'automotive-technology',
+    description: 'Трансляции про автомобили, автоспорт, ремонты, обзоры техники и гаджетов.',
+    thumbnailUrl: '/categories/automotive-technology.webp',
+  },
+  {
+    title: 'Настольные игры',
+    slug: 'board-games',
+    description: 'Игры за столом — настолки, карточные игры, DnD и ролевые приключения с друзьями.',
+    thumbnailUrl: '/categories/board-games.webp',
+  },
+  {
+    title: 'Спорт и фитнес',
+    slug: 'sports-fitness',
+    description: 'Тренировки, фитнес, йога, стримы со спортивных соревнований и советы по здоровому образу жизни.',
+    thumbnailUrl: '/categories/sports-fitness.webp',
+  },
+];
---
+++ backend/src/core/prisma/prisma.seed.ts
@@ -0,0 +1,108 @@
+import { BadRequestException, Logger } from '@nestjs/common';
+
+import { hashPassword } from '../../shared/lib/hash-password.util';
+import { prisma } from '../../shared/lib/prisma';
+
+import { CATEGORIES } from './category.entries';
+import { STREAMS_TITLE } from './stream.entries';
+import { USERNAMES } from './user.entries';
+
+async function main() {
+  try {
+    Logger.log('Заполнение базы данных');
+    await prisma.$transaction([
+      prisma.user.deleteMany(),
+      prisma.socialLink.deleteMany(),
+      prisma.stream.deleteMany(),
+      prisma.category.deleteMany(),
+    ]);
+
+    await prisma.category.createMany({
+      data: CATEGORIES,
+    });
+
+    Logger.log('Категории заполнены');
+
+    const categoriesBySlug = Object.groupBy(CATEGORIES, ({ slug }) => slug);
+    await prisma.$transaction(async (tx) => {
+      for (const username of USERNAMES) {
+        const keysOfSlug = Object.keys(categoriesBySlug);
+        const randomCategory = categoriesBySlug[keysOfSlug[Math.floor(Math.random() * keysOfSlug.length)]];
+
+        const userExists = await tx.user.findUnique({
+          where: {
+            username,
+          },
+        });
+
+        if (!userExists) {
+          const createdUser = await tx.user.create({
+            data: {
+              username,
+              displayName: username,
+              avatar: `/channels/${username}.webp`,
+              email: `${username}@example.com`,
+              password: await hashPassword('123456'),
+              isEmailVerified: true,
+              socialLinks: {
+                createMany: {
+                  data: [
+                    {
+                      title: 'Telegram',
+                      url: `https://t.me/${username}`,
+                      order: 1,
+                    },
+                    {
+                      title: 'YouTube',
+                      url: `https://www.youtube.com/${username}`,
+                      order: 2,
+                    },
+                  ],
+                },
+              },
+            },
+          });
+
+          const randomTitles = STREAMS_TITLE[randomCategory![0].slug];
+          const randomTitle = randomTitles[Math.floor(Math.random() * randomTitles.length)];
+
+          const category = await tx.category.findUnique({
+            where: {
+              slug: randomCategory![0].slug,
+            },
+          });
+
+          await tx.stream.create({
+            data: {
+              title: randomTitle,
+              thumbnailUrl: `/streams/${createdUser.username}.webp`,
+              user: {
+                connect: {
+                  id: createdUser.id,
+                },
+              },
+              category: {
+                connect: {
+                  id: category!.id,
+                },
+              },
+            },
+          });
+
+          Logger.log(`Пользователь ${createdUser.username} создан`);
+        }
+      }
+    });
+
+    Logger.log('Заполнение базы данных завершено');
+  } catch (error) {
+    Logger.error(error);
+    throw new BadRequestException('Ошибка при заполнении базы данных');
+  } finally {
+    Logger.log('Отключение от базы данных');
+    await prisma.$disconnect();
+    Logger.log('Заполнение базы данных завершено');
+  }
+}
+
+main();
